260c3b2c5c696001a07a86cd6f05dd6b1f71e447
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-09-24 00:00:57 +0300

    Updates

---
+++ precache-manifest.2863229b91bb2db8f5a7521bcb6f7b9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2710f77cbbd2507df52b500c4f518d",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "71ce653f1e34e7eabd89",
+    "url": "/react_dynamic-list-of-posts/static/css/main.5a4126be.chunk.css"
+  },
+  {
+    "revision": "3fbe971b760c9c47636f",
+    "url": "/react_dynamic-list-of-posts/static/js/2.4a464071.chunk.js"
+  },
+  {
+    "revision": "71ce653f1e34e7eabd89",
+    "url": "/react_dynamic-list-of-posts/static/js/main.a6abf878.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
